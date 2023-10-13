b9b511d00c21a2511cca2eaefd48ecee7724b96f
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-14 02:37:12 +0300

    0

--- test_cmd.py
+++ test_cmd.py
@@ -4,6 +4,8 @@ import cmd
 class Hello(cmd.Cmd):
     """Command line for the class"""
 
+    FRIENDS = ['Alice', 'Adam', 'Barbara', 'Bob']
+
     def do_hi(self, person):
         """[Hi]: Greet the named person"""
         if person:
@@ -11,10 +13,25 @@ class Hello(cmd.Cmd):
         else:
             print("HI, Welcome stranger")
 
-    
+    def help_hi(self):
+        print ('\n'.join(['greet [person]',
+                         'Greet the named person',
+                         ]))
+
     def do_exit(self, line):
-        """[Exit]: Exit the current program"""
+        """[Exit]: Exit from current session"""
+        print("Goodbye\n...\n..\n.")
         return True
+
+    def complete_hi(self, text, line, begidx, endidx):
+        if not text:
+            completions = self.FRIENDS[:]
+        else:
+            completions = [f
+                           for f in self.FRIENDS
+                           if f.startswith(text)
+                           ]
+        return completions
     
 
 if __name__ == "__main__":
