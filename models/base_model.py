e896d9e5f38bfc2968cbd3526b2e3a2d91276d9b
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-15 21:22:37 +0300

    0

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,14 @@ class BaseModel():
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-    
+        self.updated_at = datetime.now()
+
     def __str__(self):
         """return a specific format for the class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
         
-
-
-
-
