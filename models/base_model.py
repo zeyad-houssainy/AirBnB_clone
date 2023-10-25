b7e19a1c8cf80ba49a3586e9d6551f08d3d1040a
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-25 09:24:46 +0300

    0

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,50 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""Basemodel for the whole project"""
+import unittest
+import uuid
 from datetime import datetime
 
 
-class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+class BaseModel():
+    """Represents a BaseModel for airBNB clone project"""
+    def __init__(self, *agrs, **kwargs):
+        """Define for BaseModel
+            *args: not used
+            **kwargs: key:value of attributes
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """return a specific format for the class"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """changes the date time to be the current moment when click [save]"""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """create a dictionary out of the data entered"""
+        copy_dict = self.__dict__.copy()
+        copy_dict["__class__"] = self.__class__.__name__
+        # for k,v in copy_dict.items():
+        #     if k == "created_at" or k == "updated_at":
+        #         v = v.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        # print(copy_dict)
+        return (copy_dict)
+
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+# print(my_model)
+my_model.save()
+# print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+# print("JSON of my_model:")
+# for key in my_model_json.keys():
+#     print("\t{}: ({}) - {}".format(key,
+#           type(my_model_json[key]), my_model_json[key]))
